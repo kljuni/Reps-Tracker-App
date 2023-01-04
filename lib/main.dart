54559d81b6ccdaa63094735e038069ee02fc66b6
kljuni <ivankljun@gmail.com>
2023-01-04 18:59:32 +0100

    Learning basics

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print("Answer chosen");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(questions[questionIndex]),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer 1")),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer 2")),
+          ElevatedButton(onPressed: answerQuestion, child: Text("Answer 3")),
+        ],
+      ),
+    ));
+  }
+}
